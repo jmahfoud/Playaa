635d6b089e8e827b8eb1e76c7020bbc4a52f11f1
Nolyna <nsoumbou1@student.gsu.edu>
2017-04-01 18:49:02 -0400

    y

--- database.js
+++ database.js
@@ -4,16 +4,18 @@ var db = new pg.Client(process.env.DATABASE_URL);
 db.connect(function(err) {
   if (err) throw err;
   console.log('PostgreSQL is connected!');
-  db.query('CREATE TABLE IF NOT EXISTS user(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS interest(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS schedule(data JSON)');
+  db.query('CREATE TABLE IF NOT EXISTS account(userId int, email varchar(255), password varchar(255), type int)');
+  db.query('CREATE TABLE IF NOT EXISTS user(userId int, lastname varchar(255), firstname varchar(255), goal varchar(255))');
+  db.query('CREATE TABLE IF NOT EXISTS interest(interestID int, interestName varchar(255))');
+  db.query('CREATE TABLE IF NOT EXISTS schedule(scheduleId int)');
   db.query('CREATE TABLE IF NOT EXISTS posts(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS team(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS member(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS teampost(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS writepost(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS haveinterest(data JSON)');
-  db.query('CREATE TABLE IF NOT EXISTS haveschedule(data JSON)');
+  db.query('CREATE TABLE IF NOT EXISTS team(teamId int, postId int)');
+  db.query('CREATE TABLE IF NOT EXISTS member(userId int, teamId int)');
+  db.query('CREATE TABLE IF NOT EXISTS teampost(teamId int, postId int)');
+  db.query('CREATE TABLE IF NOT EXISTS writepost(postId int, userId)');
+  db.query('CREATE TABLE IF NOT EXISTS haveinterest(userId int, interestID int)');
+  db.query('CREATE TABLE IF NOT EXISTS haveschedule(userId int, scheduleId int)');
+  db.query('CREATE TABLE IF NOT EXISTS postschedule(postId int, scheduleId int)');
 });
 
 module.exports = db;
