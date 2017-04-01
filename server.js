0e74cc85d1c14aad4afa1bb02ff1272e96dbf399
Nolyna <nsoumbou1@student.gsu.edu>
2017-04-01 13:44:17 -0400

    updates

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+var db = require('./database');
+
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.listen(app.get('port'));
